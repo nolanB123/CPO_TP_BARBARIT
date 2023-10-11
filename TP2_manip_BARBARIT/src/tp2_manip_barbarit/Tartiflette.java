9d900ddcd539b8620cb97ce6c04e9538d5631d6d
Utilisateur <Utilisateur@DESKTOP-96N4K6B.education.lakanal.epf.fr>
2023-10-11 16:49:03 +0200

    Rendu du TP1 Manip

---
+++ TP2_manip_BARBARIT/src/tp2_manip_barbarit/Tartiflette.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_barbarit;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Tartiflette {
+    int nbCalories;
+    
+    public Tartiflette (int nbcal){
+        nbCalories = nbcal;
+    }
+}
